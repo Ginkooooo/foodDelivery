4cd6c26dc30932724f6db81cc21ffc2b266b757b
ginko <qhmgyyw@163.com>
2025-02-24 15:52:22 +0000

    add models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from restaurants.models import MenuItem
+from users.models import User
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(MenuItem, through='OrderItem')
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+
+class MenuItem(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
