2efbfdfaec836bfa8774fd93ff97db875817eb0a
ginko <qhmgyyw@163.com>
2025-03-11 20:55:58 +0000

    更改部分参数

--- foodDelivery/urls.py
+++ foodDelivery/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
+
 from restaurants.views import restaurant_list
 
 urlpatterns = [
     path('home/', restaurant_list, name='home'),  # 主页面
     path('restaurants/', include('restaurants.urls', namespace='restaurants')),
+    path('login/', TemplateView.as_view(template_name='login.html'), name='login'),
+    path('merchant/', TemplateView.as_view(template_name='merchant.html'), name='merchant'),
+    path('register/', TemplateView.as_view(template_name='register.html'), name='register'),
+    path('address/', TemplateView.as_view(template_name='address.html'), name='address'),
+    path('info/', TemplateView.as_view(template_name='info.html'), name='info'),
 ]
 
 if settings.DEBUG:
