d4122451838d80aee3e1765146b9af700705be45
ginko <qhmgyyw@163.com>
2025-03-11 18:24:06 +0000

    改了格式

--- foodDelivery/urls.py
+++ foodDelivery/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('home/', TemplateView.as_view(template_name='Homepage.html'), name='home'),  #主页面
     path('restaurants/', include('restaurants.urls', namespace='restaurants')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ restaurants/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Restaurant(models.Model):
+    CATEGORY_CHOICES = [
+        ('chineseFood', 'Chinese Food'),
+        ('fastFood', 'Fast Food'),
+        ('drinkAndCoffee', 'Drink And Coffee'),
+        ('sushi', 'Sushi'),
+        ('groceries', 'Groceries'),
+    ]
+
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField(default=0.0, help_text="Latitude of the restaurant")
+    longitude = models.FloatField(default=0.0, help_text="Longitude of the restaurant")
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES,
+        default='other',
+        verbose_name='Restaurant category'
+    )
+
+    image = models.ImageField(upload_to='restaurant_images/', blank=True, null=True, help_text="Restaurant image")
+
+    def __str__(self):
+        return f"{self.name} ({self.latitude}, {self.longitude})"
+
+
+class MenuItem(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
