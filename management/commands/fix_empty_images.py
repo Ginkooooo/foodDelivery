12a4984ddb824888bf70c347b6bd9c6b78418301
“Jisiyu <2990734c@student.gla.ac.uk>
2025-03-20 00:18:06 +0000

    服务器部署

---
+++ management/commands/fix_empty_images.py
@@ -0,0 +1,15 @@
+# your_app/management/commands/fix_empty_images.py
+from django.core.management.base import BaseCommand
+from restaurants.models import Restaurant
+
+class Command(BaseCommand):
+    help = 'Fix empty image fields by setting a default image path'
+
+    def handle(self, *args, **options):
+        restaurants = Restaurant.objects.filter(image__isnull=True)
+        count = 0
+        for restaurant in restaurants:
+            restaurant.image = 'defaults/default.png'
+            restaurant.save()
+            count += 1
+        self.stdout.write(f'Updated {count} restaurants.')
